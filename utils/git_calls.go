7d271df66423881718e75007f8e35df5d51a5fa5
Bo Maryniuk <bo@maryniuk.net>
2020-08-07 16:26:26 +0200

    Implement current branch getter

--- utils/git_calls.go
+++ utils/git_calls.go
@@ -1,6 +1,9 @@
 package gsc_utils
 
 import (
+	"bytes"
+	"strings"
+
 	wzlib_logger "github.com/infra-whizz/wzlib/logger"
 	wzlib_subprocess "github.com/infra-whizz/wzlib/subprocess"
 )
@@ -23,4 +26,13 @@ func (gitcall *GitCaller) Call(args ...string) {
 	if err != nil {
 		gitcall.GetLogger().Errorf("Error completing Git call: %s", err.Error())
 	}
+}
+
+func (gitcall *GitCaller) GetCurrentBranch() string {
+
+	cmd := wzlib_subprocess.ExecCommand("git", "symbolic-ref", "--short", "HEAD")
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	cmd.Run()
+	return strings.TrimSpace(out.String())
 }
