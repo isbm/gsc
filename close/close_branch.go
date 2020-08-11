1e84c572c5bc0b07057688395080e69160cbaf66
Bo Maryniuk <bo@maryniuk.net>
2020-08-11 16:52:23 +0200

    Prevent closing the main branch

--- close/close_branch.go
+++ close/close_branch.go
@@ -1,12 +1,16 @@
 package gsc_close
 
 import (
+	"fmt"
+
+	wzlib_logger "github.com/infra-whizz/wzlib/logger"
 	wzlib_subprocess "github.com/infra-whizz/wzlib/subprocess"
 	gsc_utils "github.com/isbm/gsc/utils"
 )
 
 type GSCCloseBranch struct {
 	git *gsc_utils.GitCaller
+	wzlib_logger.WzLogger
 }
 
 func NewGSCCloseBranch() *GSCCloseBranch {
@@ -18,6 +22,12 @@ func NewGSCCloseBranch() *GSCCloseBranch {
 // Close current branch and delete it
 func (cb *GSCCloseBranch) Close() error {
 	currentBranch := cb.git.GetCurrentBranch()
+	defaultBranch := cb.git.GetDefaultBranch()
+	if currentBranch == defaultBranch {
+		err := fmt.Errorf("Cannot close main branch")
+		cb.GetLogger().Error(err.Error())
+		return err
+	}
 	if err := wzlib_subprocess.ExecCommand("git", "checkout", cb.git.GetDefaultBranch()).Run(); err != nil {
 		return err
 	}
