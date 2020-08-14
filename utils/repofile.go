c83906fcc58cf15e4a37b434d0f71327e4f0670a
Bo Maryniuk <bo@maryniuk.net>
2020-08-14 18:55:59 +0200

    Add XML serialisation to the repo file

--- utils/repofile.go
+++ utils/repofile.go
@@ -2,6 +2,7 @@ package gsc_utils
 
 import (
 	"encoding/xml"
+	"fmt"
 )
 
 /*
@@ -10,6 +11,7 @@ import (
 	<?xml version="1.0" encoding="UTF-8"?>
 	<git>
 	  <url>git@github.com:somebody/my_great_package.git</url>
+	  <branch>release-1.2</branch>
 	</git>
 */
 var GIT_PKG_REPO string = "_git_pkg_repo"
@@ -17,4 +19,22 @@ var GIT_PKG_REPO string = "_git_pkg_repo"
 type GitPkgRepo struct {
 	XMLName xml.Name `xml:"git"`
 	Url     string   `xml:"url"`
+	Branch  string   `xml:"branch"`
+	isNew   bool
+}
+
+// SetIsNew to true if the repo XML file was just created
+func (gpr *GitPkgRepo) SetIsNew() {
+	gpr.isNew = true
+}
+
+// IsNew returns true if repo file was just created
+func (gpr *GitPkgRepo) IsNew() bool {
+	return gpr.isNew
+}
+
+// ToXML serialisation
+func (gpr *GitPkgRepo) ToXML() string {
+	return fmt.Sprintf("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<git>\n  <url>%s</url>\n  <branch>%s</branch>\n</git>\n",
+		gpr.Url, gpr.Branch)
 }
